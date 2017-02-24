21055b84538295d9c216ca4871e98fa6d83ab482
sashidharan <sashi.it4@gmail.com>
2017-02-24 16:17:51 +0530

    Startup

---
+++ www/js/controllers.js
@@ -0,0 +1,109 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, $rootScope, $state, Chats, AjaxMethod, $ionicHistory) {
+    /*$rootScope.goHome = function() {
+        $state.go('home');
+        $ionicHistory.clearHistory();
+    };*/
+    $scope.articles = Chats.all();
+    $scope.redirect = function(urlRequest) {
+    $state.go(urlRequest.url, {siteId:urlRequest.siteId});
+    };
+})
+
+.controller('scanCtrl', function($scope, $state, $stateParams, AjaxMethod, $cordovaBarcodeScanner, $ionicLoading) {
+    $scope.scanBarcode = function() {
+        // AjaxMethod.loadUrl($stateParams.siteId, 30061984);
+        // $state.go('info');
+        $cordovaBarcodeScanner
+            .scan()
+            .then(function(barcodeData) {
+                // alert(barcodeData.text+'---'+barcodeData.format+'----'+barcodeData.cancelled);
+                AjaxMethod.loadUrl($stateParams.siteId, barcodeData.text);
+                $state.go('info');
+                // console.log(barcodeData);
+            }, function(error) {
+                console.log('erroe->', error)
+                // An error occurred
+        });
+        // NOTE: encoding not functioning yet
+        /*$cordovaBarcodeScanner
+            .encode(BarcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com")
+            .then(function(success) {
+                // Success!
+                console.log('encode---->',success)
+            }, function(error) {
+                // An error occurred
+        });*/
+    }
+})
+
+.controller('infoCtrl', function($scope, $ionicModal, $sce, AjaxMethod) {
+    $scope.articles = {};
+
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function(modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.loadData = function () {
+        AjaxMethod.getMethod('', function (result) {
+            if(result) {
+                $scope.articles = {
+                    story: result.body,
+                    img: result.images,
+                    cmts: result.comments,
+                    author: result.author,
+                    heading: result.headline
+                }                
+            } else {
+                alert('Invalid QR_Code');
+            }
+        });
+    };
+
+    $scope.openModal = function(data) {
+        $scope.dataType = '';
+        if($scope.articles.hasOwnProperty(data)) {
+            $scope.dataType = data;
+            $scope.details = '';
+            switch(data) {
+                case 'story':
+                    $scope.details = $sce.trustAsHtml($scope.articles[data]);
+                break;
+                case 'img':
+                    if($scope.articles[data].length > 0) {
+                        $scope.details = $scope.articles[data];
+                    }
+                break;
+                case 'cmts':
+                    if($scope.articles[data].length > 0) {
+                        $scope.details = $scope.articles[data];
+                    }
+                break;
+                default:
+                break;
+            }
+            $scope.modal.show();
+        } else {
+            console.log('erroe');
+        }
+    };
+    $scope.closeModal = function() {
+        $scope.modal.hide();
+    };
+    // Cleanup the modal when we're done with it!
+    $scope.$on('$destroy', function() {
+        $scope.modal.remove();
+    });
+    // Execute action on hide modal
+    $scope.$on('modal.hidden', function() {
+    // Execute action
+    });
+    // Execute action on remove modal
+    $scope.$on('modal.removed', function() {
+    // Execute action
+    });
+});
---
+++ www/js/services.js
@@ -0,0 +1,82 @@
+angular.module('starter.services', [])
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Bristol',
+    url: 'scan',
+    style: 'bristol',
+    siteId: 276268,
+    img: 'img/bristol.png'
+  }, {
+    id: 1,
+    name: 'Notting Ham',
+    url: 'info',
+    style: 'hulldaily',
+    siteId: 568577,
+    img: 'img/nottingham.png'
+  }, {
+    id: 2,
+    name: 'Hull Daily Mail',
+    url: 'info',
+    style: 'nottingham',
+    siteId: 568577,
+    img: 'img/hulldaily.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    }
+  };
+})
+.factory('AjaxMethod',  ['$http', function($http) {
+    var urlBase = 'https://api.localworld.co.uk/articles/%SITE_ID%/detail/%ARTICLE_ID%';
+    return {
+        genUrl: '',
+        callmethod:function(){
+            alert('haide');
+        },
+        loadUrl: function (siteId, articleId) {
+            this.genUrl = urlBase.replace(/%SITE_ID%/g, siteId);
+            this.genUrl = this.genUrl.replace(/%ARTICLE_ID%/g, articleId);
+            console.log(this.genUrl);
+        },
+        getMethod:function (url, callback) {
+            console.log(this.genUrl);
+            try {
+                $http.get(this.genUrl)
+                /*.then(function (response) {
+                    console.log('response--->', response)
+                    return response;
+                });*/
+                .success(function (response) {
+                    callback(response);
+                    return response;
+                })
+                .error(function(err) {
+                    callback(err);
+                    console.log(err);
+                });
+            } catch(e) {
+                console.log(e);
+            }
+        },
+        postMethod:function (parameter) {
+            try {
+                $http.post(urlBase, parameter)
+                .success(function (response) {
+                    return response;
+                })
+                .error(function(err) {
+                    console.log(err);
+                });
+            } catch(e) {
+                console.log(e);
+            }
+        }
+    };
+}]);
